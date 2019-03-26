5a45cbe041b67b949a7110d1d45fb9b1935265e8
Monteth <greezlie@gmail.com>
2019-03-26 19:38:47 +0100

    Add schema req

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const expect = require('chai').expect;
 const graphqlHTTP = require('express-graphql');
-// const schema = require('./schema/schema.js');
+const schema = require('./schema/schema.js');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://user1:ZZQLuMDv5dOJEEmB@cluster0-jhmtv.mongodb.net/postalpigeon?retryWrites=true');
@@ -12,7 +12,7 @@ mongoose.connection.once('open', () => {
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-    // schema: schema,
+    schema: schema,
     graphiql: true
 }));
 
